40e6fa0c496919ca3a6f18a5edebfa0770132a3a
isudox <isudox@gmail.com>
2020-10-18 22:53:54 +0800

    publish 20201018

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+cd public
+git checkout master
+cd ..
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
